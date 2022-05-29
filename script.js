585e1b7c6d4f3c0d239e49089ad2444916d47227
Liao Zhu <liao.zhu.cs@gmail.com>
2022-05-28 20:47:00 -0500

    got checkboxes

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+let noteValue = document.getElementById("note-input-id");
+
+function addNoteToList() {
+    let addNoteValue = noteValue.value;
+    if(addNoteValue != "") {
+        let unLi = document.getElementById("ul-id");
+        let newLi = document.createElement("li");
+        let checkbox = document.createElement('input');
+        checkbox.type = "checkbox";
+        newLi.appendChild(checkbox);
+        newLi.appendChild(document.createTextNode(addNoteValue));
+        unLi.appendChild(newLi);
+    }
+    noteValue.value = "";
+}
+    
 
 
-let addBtn = document.getElementById("add-button");
+
+
+
+
+
+
+document.getElementById("add-button-id").addEventListener("click", addNoteToList);
