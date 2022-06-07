9adc32c875f6c72c57e01e7459dda7bcaf10c004
Liao Zhu <liao.zhu.cs@gmail.com>
2022-06-06 22:58:44 -0500

    worked on styling notes

--- script.js
+++ script.js
@@ -16,20 +16,20 @@ function displayItem(todoItem) {
     // create item wrapper
     //
     let ItemDiv = document.createElement("div");
-    ItemDiv.className = "todo-item-wrapper";
+    ItemDiv.className = "item-wrapper-class";
     ItemDiv.setAttribute("data-id", todoItem.id);
     ItemDiv.setAttribute("id", todoItem.id);
 
     // create text, checkbox, delete button for the item
     //
     let ItemText = document.createElement("p");
-    ItemText.className = "item-text-class";
+    ItemText.className = "note-text-class";
     ItemText.innerHTML = todoItem.text;
     ItemText.setAttribute("id", "p-"+todoItem.id);
 
     let ItemBox = document.createElement("input");
     ItemBox.type = "checkbox";
-    ItemBox.className = "complete-checkbox-class";
+    ItemBox.className = "checkbox-class";
     ItemBox.setAttribute("data-id", todoItem.id);
     ItemBox.addEventListener("click", setComplete);
 
@@ -45,8 +45,8 @@ function displayItem(todoItem) {
 
     let ItemDelBtn = document.createElement("input");
     ItemDelBtn.type = "button";
-    ItemDelBtn.className = "delete-button-class";
-    ItemDelBtn.value = "delete";
+    ItemDelBtn.className = "delete-class";
+    ItemDelBtn.value = "X";
     ItemDelBtn.setAttribute("data-id", todoItem.id);
     ItemDelBtn.addEventListener("click", deleteItem);
 
